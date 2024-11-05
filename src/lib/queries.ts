33bc5bc0a95c03cb783167617d5cea4a4c320bbe
tonipcv <xppsalvador@gmail.com>
2024-11-05 15:34:44 -0300

    agora ultimo

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,4 +1,12 @@
-import prisma from '@/lib/prisma'
+import prisma from './prisma'
+
+interface Trade {
+  data: Date;
+  ativo: string;
+  direcao: string;
+  percentual: number | null;
+  alvo: string;
+}
 
 export async function getTradesByMonth(month: number) {
   return await prisma.trade.findMany({
@@ -14,20 +22,16 @@ export async function getTradesByMonth(month: number) {
   })
 }
 
-export async function getTradeStats(month?: number) {
-  const whereClause = month ? {
-    data: {
-      gte: new Date(2024, month - 1, 1),
-      lt: new Date(2024, month, 1)
-    }
-  } : {}
-
-  const trades = await prisma.trade.findMany({ where: whereClause })
-  
+export async function getTradeStats(trades: Trade[]) {
   const totalOperacoes = trades.length
-  const operacoesLucrativas = trades.filter(t => t.percentual > 0).length
+  
+  // Garante que percentual não seja nulo
+  const operacoesLucrativas = trades.filter(t => (t.percentual ?? 0) > 0).length
+  
   const taxaAcerto = totalOperacoes > 0 ? ((operacoesLucrativas / totalOperacoes) * 100) : 0
-  const valorizacaoTotal = trades.reduce((acc, curr) => acc + curr.percentual, 0)
+  
+  // Usa o operador nullish coalescing para garantir um valor padrão
+  const valorizacaoTotal = trades.reduce((acc, curr) => acc + (curr.percentual ?? 0), 0)
 
   return {
     totalOperacoes,
